458d5c6e75f50a7562f6a721ba95a8ae3fbed1e2
Xinyu Liu <liuxinyujames@gmail.com>
2025-11-09 18:25:19 -0500

    update

--- src/MAX30205.c
+++ src/MAX30205.c
@@ -1,5 +1,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 #define SDA_PIN (26)
 #define SCL_PIN (27)
@@ -10,28 +12,32 @@ void MAX30205_init_i2c()
     gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
 
-    i2c_init(i2c1_hw, 125000);
+    i2c_init(i2c1, 125000);
     gpio_pull_up(SDA_PIN);
     gpio_pull_up(SCL_PIN);
-
-    printf("Scanning I2C bus...\n");
-    for (uint8_t addr = 0x08; addr < 0x78; addr++) {
-        uint8_t dummy;
-        int result = i2c_read_blocking(i2c1_hw, addr, &dummy, 1, false);
-        if (result >= 0)
-            printf("Found device at 0x%02X\n", addr);
-    }
-    printf("Scan complete.\n");
 }
 
 void MAX30205_check_address()
 {
-    printf("scanning I2C bus for MAX30205...\n");
-    for (uint8_t addr = 0x40; addr < 0x7F; addr++)
-    {
+    printf("scanning I2C bus for MAX30205...\n"); fflush(stdout);
+    
+    for (uint8_t addr = 0x08; addr < 0x7F; addr++)
+    {   
+        uint8_t reg=0x00;
+        i2c_write_blocking(i2c1, addr, &reg, 1, true);
+
         uint8_t result;
-        int read = i2c_read_blocking(i2c1_hw, addr, &result, 1, false);
-        if (read >= 0) printf("I2C found at 0x%02X\n", addr); 
+        int read = i2c_read_blocking(i2c1, addr, &result, 1, false);
+        if (read >= 0)
+        {
+            printf("I2C found at 0x%02X\n", addr); fflush(stdout);
+            sleep_ms(100);
+        } 
+        else
+        {
+            printf("Failed at 0x%X\r\n", addr); fflush(stdout);
+            sleep_ms(100);
+        } 
     }
-    printf("Scanning End...\n");
+    printf("Scanning End...\n"); fflush(stdout);
 }
--- src/main.c
+++ src/main.c
@@ -1,11 +1,20 @@
+#include "pico/stdlib.h"
+#include "pico/stdio_usb.h"
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
+#include <string.h>
+#include "hardware/i2c.h"
 #include "MAX30205.h"
 
 #define MAX30205_TEST
 
 int main()
 {
+    stdio_init_all();
+    sleep_ms(3000);
+    printf("START...\n");
+    fflush(stdout);
 
     #ifdef MAX30205_TEST
     MAX30205_init_i2c();
@@ -13,5 +22,6 @@ int main()
 
     #endif
 
+    for(;;);    
     return 0;
 }
