0a4d6b5823f9a0e62c43db3af11d5fa2b81a201f
clark952 <clark952@purdue.edu>
2025-12-02 10:20:55 -0500

    Update and rename MAX30205.h to MAX30102.h

---
+++ src/MAX30102.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+
+#include "hardware/gpio.h"
+#include "hardware/irq.h"
+#include "hardware/timer.h"
+#include "hardware/adc.h"
+#include "hardware/dma.h"
+#include "hardware/pwm.h"
+#include "hardware/spi.h"
+#include "hardware/uart.h"
+#include "pico/rand.h"
+#include "hardware/i2c.h"
